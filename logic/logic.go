dde52e86ab4424f290747ce9c6c054ee0d78603f
zemanzeng <zemanzeng@tencent.com>
2022-02-04 23:30:39 +0800

    --feat=leetcode 329、365

--- logic/logic.go
+++ logic/logic.go
@@ -12,6 +12,7 @@ package logic
 
 import (
 	"container/list"
+	"strconv"
 )
 
 // leetcode 20:https://leetcode.com/problems/valid-parentheses/
@@ -161,4 +162,149 @@ func SearchInRotatedSortedArray(nums []int, target int) int {
 	}
 
 	return -1
+}
+
+// leetcode 329: https://leetcode.com/problems/longest-increasing-path-in-a-matrix/
+func LongestIncreasingPath(matrix [][]int) int {
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return 0
+	}
+
+	m, n := len(matrix), len(matrix[0])
+
+	list := make([]int, 0)
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			list = append(list, matrix[i][j])
+		}
+	}
+
+	var findLongest func(matrix [][]int, from int) int
+
+	memo := make(map[int]int)
+	findLongest = func(matrix [][]int, index int) int {
+		if dist, exist := memo[index]; exist {
+			return dist
+		}
+
+		// 上 下
+		indexs := []int{index - n, index + n}
+
+		row := index % n
+		if row-1 >= 0 { // 左
+			indexs = append(indexs, index-1)
+		}
+		if row+1 < n { // 右
+			indexs = append(indexs, index+1)
+		}
+
+		var max int = 1
+		for _, nextIndex := range indexs {
+			if nextIndex >= 0 && nextIndex < len(list) &&
+				list[nextIndex] > list[index] {
+				if dist := findLongest(matrix, nextIndex) + 1; dist > max {
+					max = dist
+				}
+			}
+		}
+
+		memo[index] = max
+		return max
+	}
+
+	max, index := 0, -1
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			index = index + 1
+
+			if dist := findLongest(matrix, index); dist > max {
+				max = dist
+			}
+		}
+	}
+	return max
+}
+
+// GCD: Greatest common divisor(最大公约数)
+func MaxGCD(m, n int) int {
+	mod := m % n
+	if mod == 0 {
+		return n
+	}
+	return MaxGCD(n, mod)
+}
+
+// leetcode 365: https://leetcode.com/problems/water-and-jug-problem/
+// 本质上是一个加减法问题 需要注意判断无效(死循环-doingMemo、永远添加不到-最大公约数)
+// 优化: getMaxGCD缓存、失败记录缓存
+func CanMeasureWater(jug1Capacity int, jug2Capacity int, targetCapacity int) bool {
+
+	gcdmemo := make(map[string]int)
+	getMaxGCD := func(a int, b int) int {
+		if a < b {
+			a, b = b, a
+		}
+		key := strconv.Itoa(a) + "_" + strconv.Itoa(b)
+		if gcd, exist := gcdmemo[key]; exist {
+			return gcd
+		}
+		gcd := MaxGCD(a, b)
+		gcdmemo[key] = gcd
+		return gcd
+	}
+
+	var measure func(jug1 int, jug2 int, target int) bool
+
+	succMemo := make(map[int]bool) // false: failed
+	doingMemo := make(map[int]bool)
+	measure = func(jug1 int, jug2 int, target int) bool {
+		if target < 0 || jug1+jug2 < target {
+			return false
+		}
+		if succ, exist := succMemo[target]; exist {
+			return succ
+		}
+		if target == 0 || jug1 == target || jug2 == target || jug1+jug2 == target ||
+			jug1-jug2 == target || jug2-jug1 == target {
+			succMemo[target] = true
+			return true
+		}
+
+		gcd := getMaxGCD(jug1, jug2)
+		if gcd == 1 && targetCapacity == 1 {
+			succMemo[target] = true
+			return true
+		}
+		if gcd > 1 && target%gcd != 0 { // 这种情况倒来倒去始终为最大公约数的倍数
+			succMemo[target] = false
+			return false
+		}
+
+		left1, left2 := jug1-target, jug2-target
+		if left1 < 0 {
+			left1 = -left1
+		}
+		if left2 < 0 {
+			left2 = -left2
+		}
+
+		result := false
+		if _, exist := succMemo[left1]; !exist { // 过滤已经失败的，性能优化
+			if _, exist := doingMemo[left1]; !exist { // 过滤掉正在做的，规避循环调用
+				doingMemo[left1] = true
+				result = measure(jug1, jug2, left1)
+			}
+		}
+		if _, exist := succMemo[left2]; !exist {
+			if _, exist := doingMemo[left2]; !exist {
+				doingMemo[left2] = true
+				result = result || measure(jug1, jug2, left2)
+			}
+		}
+
+		succMemo[target] = result
+		return result
+	}
+
+	return measure(jug1Capacity, jug2Capacity, targetCapacity)
 }
--- logic/logic_test.go
+++ logic/logic_test.go
@@ -178,3 +178,62 @@ func TestSearch(t *testing.T) {
 	}
 
 }
+
+func TestLongestIncreasingPath(t *testing.T) {
+	type longestPathResult struct {
+		matrix [][]int
+		output int
+	}
+
+	results := []*longestPathResult{
+		{
+			matrix: [][]int{
+				{9, 9, 4},
+				{6, 6, 8},
+				{2, 1, 1},
+			},
+			output: 4,
+		},
+		{
+			matrix: [][]int{
+				{6, 8},
+				{7, 2},
+			},
+			output: 2,
+		},
+	}
+	for _, result := range results {
+		if output := LongestIncreasingPath(result.matrix); output != result.output {
+			t.Errorf("longest_increasing_path result:%+v output:%v", result, output)
+		}
+	}
+
+}
+
+func TestCanMeasureWater(t *testing.T) {
+	type measureResult struct {
+		jug1   int
+		jug2   int
+		target int
+		output bool
+	}
+
+	results := []*measureResult{
+		{jug1: 3, jug2: 5, target: 4, output: true},
+		{jug1: 2, jug2: 6, target: 5, output: false},
+		{jug1: 1, jug2: 2, target: 3, output: true},
+		{jug1: 9, jug2: 6, target: 1, output: false},
+		{jug1: 10000, jug2: 10001, target: 1, output: true},
+		{jug1: 4, jug2: 6, target: 8, output: true},
+		{jug1: 34, jug2: 5, target: 6, output: true},
+		{jug1: 1, jug2: 1, target: 12, output: false},
+	}
+
+	for _, result := range results {
+		output := CanMeasureWater(result.jug1, result.jug2, result.target)
+		if output != result.output {
+			t.Errorf("can_measure_water result:%+v output:%v", result, output)
+		}
+	}
+
+}
