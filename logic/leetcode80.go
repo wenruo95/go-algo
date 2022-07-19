649027a37e8c9a27477320c05837b0bb37a750e2
wenruo95 <1358124684@qq.com>
2022-07-19 23:25:31 +0800

    --feat=leetcode 81

--- logic/leetcode80.go
+++ logic/leetcode80.go
@@ -18,3 +18,36 @@ func RemoveDuplicates2(nums []int) int {
 	}
 	return len(nums) - k
 }
+
+// leetcode 81: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+func SearchInRotatedSortedArrayII(nums []int, target int) bool {
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return true
+		}
+
+		if nums[left] == nums[mid] && nums[mid] == nums[right] {
+			left = left + 1
+			right = right - 1
+		} else if nums[left] <= nums[mid] {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+
+	}
+
+	return false
+}
--- logic/leetcode80_test.go
+++ logic/leetcode80_test.go
@@ -35,3 +35,59 @@ func TestRemoveDuplicates2(t *testing.T) {
 	}
 
 }
+
+func TestSearchInRotateArrayII(t *testing.T) {
+	type testData struct {
+		nums        []int
+		trueTarget  []int
+		falseTarget []int
+	}
+
+	datas := []*testData{
+		{
+			nums:        []int{2, 5, 6, 0, 0, 1, 2},
+			trueTarget:  []int{0, 1, 2, 5, 6},
+			falseTarget: []int{-1, -2, -3, 3, 4, 7, 8, 9, 10},
+		},
+		{
+			nums:        []int{5, 2},
+			trueTarget:  []int{2, 5},
+			falseTarget: []int{-1, -2, -3, 3, 4, 7, 8, 9, 10},
+		},
+		{
+			nums:        []int{2, 5},
+			trueTarget:  []int{2, 5},
+			falseTarget: []int{-1, -2, -3, 3, 4, 7, 8, 9, 10},
+		},
+		{
+			nums:        []int{2},
+			trueTarget:  []int{2},
+			falseTarget: []int{-1, -2, -3, 3, 4, 5, 7, 8, 9, 10},
+		},
+		{
+			nums:        []int{1, 0, 1, 1, 1},
+			trueTarget:  []int{0, 1},
+			falseTarget: []int{-1, -2, -3, 2, 3, 4},
+		},
+		{
+			nums:        []int{5, 1, 3},
+			trueTarget:  []int{5, 3, 1},
+			falseTarget: []int{-1, -2, -3, 0, 2, 4, 6, 7, 8},
+		},
+	}
+
+	for _, data := range datas {
+		for _, target := range data.trueTarget {
+			if exist := SearchInRotatedSortedArrayII(data.nums, target); !exist {
+				t.Errorf("search error. nums:%v target:%v exist:%v expect:true", data.nums, target, exist)
+			}
+		}
+		for _, target := range data.falseTarget {
+			if exist := SearchInRotatedSortedArrayII(data.nums, target); exist {
+				t.Errorf("search error. nums:%v target:%v exist:%v expect:false", data.nums, target, exist)
+			}
+		}
+
+	}
+
+}
