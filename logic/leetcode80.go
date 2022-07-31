4c5f5c1e081c959c086ec15e3f0a0276474cf91a
wenruo95 <1358124684@qq.com>
2022-07-31 23:29:37 +0800

    --feat=leetcode 85

--- logic/leetcode80.go
+++ logic/leetcode80.go
@@ -1,5 +1,10 @@
 package logic
 
+import (
+	"fmt"
+	"strconv"
+)
+
 // leetcode 80: https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
 func RemoveDuplicates2(nums []int) int {
 	var count, k int
@@ -73,4 +78,71 @@ func LargestRectangleArea(heights []int) int {
 		stack.Push(i)
 	}
 	return area
+}
+
+// leetcode 85: https://leetcode.com/problems/maximal-rectangle/
+func MaximalRectangle(matrix [][]byte) int {
+	var (
+		columns  = make(map[string]int)
+		columndp func(row, column int) int
+		rows     = make(map[string]int)
+		rowsdp   func(row, column int) int
+	)
+	columndp = func(row, column int) int {
+		if row >= len(matrix) {
+			return 0
+		}
+
+		key := strconv.Itoa(row) + "_" + strconv.Itoa(column)
+		if v, exist := columns[key]; exist {
+			return v
+		}
+
+		var result int
+		if matrix[row][column] == '1' {
+			result = 1 + columndp(row+1, column)
+		} else {
+			result = 0
+		}
+		columns[key] = result
+		return result
+	}
+	rowsdp = func(row, column int) int {
+		if column >= len(matrix[0]) {
+			return 0
+		}
+
+		key := strconv.Itoa(row) + "_" + strconv.Itoa(column)
+		if v, exist := rows[key]; exist {
+			return v
+		}
+
+		var result int
+		if matrix[row][column] == '1' {
+			result = 1 + rowsdp(row, column+1)
+		} else {
+			result = 0
+		}
+		rows[key] = result
+		return result
+	}
+
+	var max int
+	for row := 0; row < len(matrix); row++ {
+		for column := 0; column < len(matrix[0]); column++ {
+			maxRow := rowsdp(row, column)
+
+			var minColumn int = len(matrix) + 1
+			for width := 1; width <= maxRow && matrix[row][column+width-1] != 0; width++ {
+				minColumn = intMin(minColumn, columndp(row, column+width-1))
+				max = intMax(max, width*minColumn)
+			}
+			fmt.Printf("[TEST0] row:%v column:%v max_row:%v min_column:%v max:%v\n",
+				row, column, maxRow, minColumn, max)
+
+		}
+	}
+	fmt.Printf("[TEST9] rows:%v columns:%v max:%v\n", jsonstr(rows), jsonstr(columns), max)
+
+	return max
 }
