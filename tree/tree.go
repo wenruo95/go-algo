6c88a0428eb7f01031757c85950045b5d2785b89
wenruo95 <1358124684@qq.com>
2022-08-28 23:27:34 +0800

    --feat=leetcode 95

--- tree/tree.go
+++ tree/tree.go
@@ -10,41 +10,35 @@
 
 package tree
 
+import (
+	"fmt"
+)
+
 type TreeNode struct {
-	Val   interface{}
+	Val   int
 	Left  *TreeNode
 	Right *TreeNode
 }
 
-func New(val interface{}) *TreeNode {
-	if val == nil {
+func NewTreeNodes(vals ...int) *TreeNode {
+	if len(vals) == 0 {
 		return nil
 	}
 
-	node := new(TreeNode)
-	node.Val = val
-	return node
-}
-
-func NewTreeNodes(values ...interface{}) *TreeNode {
-	if len(values) == 0 {
-		return nil
+	nodes := make([]*TreeNode, len(vals))
+	for i := 0; i < len(vals); i++ {
+		nodes[i] = &TreeNode{Val: vals[i]}
 	}
 
-	nodes := make([]*TreeNode, len(values))
-	for i := 0; i < len(values); i++ {
-		nodes[i] = New(values[i])
-	}
-
-	for i := 0; i < len(values); i++ {
+	for i := 0; i < len(vals); i++ {
 		if nodes[i] == nil {
 			continue
 		}
 
-		if leftIndex := 2*i + 1; leftIndex < len(values) && nodes[leftIndex] != nil {
+		if leftIndex := 2*i + 1; leftIndex < len(vals) && nodes[leftIndex] != nil {
 			nodes[i].Left = nodes[leftIndex]
 		}
-		if rightIndex := 2*i + 2; rightIndex < len(values) && nodes[rightIndex] != nil {
+		if rightIndex := 2*i + 2; rightIndex < len(vals) && nodes[rightIndex] != nil {
 			nodes[i].Right = nodes[rightIndex]
 		}
 	}
@@ -52,38 +46,118 @@ func NewTreeNodes(values ...interface{}) *TreeNode {
 	return nodes[0]
 }
 
-func PreorderTraversal(root *TreeNode) []interface{} {
+func PreorderTraversal(root *TreeNode) []int {
 	if root == nil {
 		return nil
 	}
 
-	result := make([]interface{}, 0)
+	result := make([]int, 0)
 	result = append(result, root.Val)
 	result = append(result, PreorderTraversal(root.Left)...)
 	result = append(result, PreorderTraversal(root.Right)...)
 	return result
 }
 
-func InorderTraversal(root *TreeNode) []interface{} {
+func InorderTraversal(root *TreeNode) []int {
 	if root == nil {
 		return nil
 	}
 
-	result := make([]interface{}, 0)
+	result := make([]int, 0)
 	result = append(result, InorderTraversal(root.Left)...)
 	result = append(result, root.Val)
 	result = append(result, InorderTraversal(root.Right)...)
 	return result
 }
 
-func PostorderTraversal(root *TreeNode) []interface{} {
+func PostorderTraversal(root *TreeNode) []int {
 	if root == nil {
 		return nil
 	}
 
-	result := make([]interface{}, 0)
+	result := make([]int, 0)
 	result = append(result, PostorderTraversal(root.Left)...)
 	result = append(result, PostorderTraversal(root.Right)...)
 	result = append(result, root.Val)
 	return result
+}
+
+// leetcode 95: https://leetcode.com/problems/unique-binary-search-trees-ii/
+func GenerateTrees(n int) []*TreeNode {
+	set := make(map[int]struct{})
+	for i := 1; i <= n; i++ {
+		set[i] = struct{}{}
+	}
+
+	var fn func() [][]int
+	fn = func() [][]int {
+		if len(set) <= 1 {
+			var val int
+			for item := range set {
+				val = item
+			}
+			return [][]int{{val}}
+		}
+
+		result := make([][]int, 0)
+		for val := 1; val <= n; val++ {
+			if _, exist := set[val]; !exist {
+				continue
+			}
+
+			delete(set, val)
+			for _, v2 := range fn() {
+				list := append([]int{val}, v2...)
+				result = append(result, list)
+			}
+			set[val] = struct{}{}
+		}
+
+		return result
+	}
+
+	keys := make(map[string]struct{})
+	nodes := make([]*TreeNode, 0)
+	for _, v2 := range fn() {
+		node := GenerateBinarySearchTree(v2...)
+
+		key := fmt.Sprintf("%+v %+v %+v", PreorderTraversal(node), InorderTraversal(node), PostorderTraversal(node))
+		if _, exist := keys[key]; exist {
+			continue
+		}
+		keys[key] = struct{}{}
+		nodes = append(nodes, node)
+	}
+	return nodes
+}
+
+func GenerateBinarySearchTree(vals ...int) *TreeNode {
+	if len(vals) == 0 {
+		return nil
+	}
+
+	root := &TreeNode{Val: vals[0]}
+	for i := 1; i < len(vals); i++ {
+		node := root
+		for {
+
+			if vals[i] > node.Val {
+				if node.Right == nil {
+					node.Right = &TreeNode{Val: vals[i]}
+					break
+				}
+				node = node.Right
+				continue
+			}
+
+			if node.Left == nil {
+				node.Left = &TreeNode{Val: vals[i]}
+				break
+			}
+			node = node.Left
+		}
+
+	}
+
+	return root
 }
--- tree/tree_test.go
+++ tree/tree_test.go
@@ -10,114 +10,21 @@
 
 package tree
 
-import (
-	"reflect"
-	"testing"
-)
+import "testing"
 
-func listInterfaceEqual(a []interface{}, b []interface{}) bool {
-	if len(a) != len(b) {
-		return false
+func TestGenerateTrees(t *testing.T) {
+	m := map[int]int{
+		1: 1,
+		3: 5,
 	}
-	for i := 0; i < len(a); i++ {
-		if reflect.TypeOf(a[i]) != reflect.TypeOf(b[i]) ||
-			reflect.ValueOf(a[i]) != reflect.ValueOf(b[i]) {
-			return false
+	for n, length := range m {
+		nodes := GenerateTrees(n)
+		if len(nodes) != length {
+			t.Errorf("generate_trees n:%v len:%v expected:%v", n, len(nodes), length)
+			continue
+		}
+		for index, node := range nodes {
+			t.Logf("k:%v index:%v pre_order:%+v", n, index, PreorderTraversal(node))
 		}
 	}
-	return true
-}
-
-type orderResult struct {
-	input  []interface{}
-	output []interface{}
-}
-
-func TestPreorderTraversal(t *testing.T) {
-
-	results := []*orderResult{
-		{
-			input:  []interface{}{1, nil, 2, nil, nil, 3},
-			output: []interface{}{1, 2, 3},
-		},
-		{
-
-			input:  []interface{}{1},
-			output: []interface{}{1},
-		},
-		{
-
-			input:  []interface{}{},
-			output: []interface{}{},
-		},
-	}
-
-	for _, result := range results {
-		root := NewTreeNodes(result.input...)
-		orderList := PreorderTraversal(root)
-		if !listInterfaceEqual(orderList, result.output) {
-			t.Errorf("pre_order_traversal not qual. input:%+v orderlist:%+v output:%v",
-				result.input, orderList, result.output)
-		}
-	}
-
-}
-
-func TestInorderTraversal(t *testing.T) {
-	results := []*orderResult{
-		{
-			input:  []interface{}{1, nil, 2, nil, nil, 3},
-			output: []interface{}{1, 3, 2},
-		},
-		{
-
-			input:  []interface{}{1},
-			output: []interface{}{1},
-		},
-		{
-
-			input:  []interface{}{},
-			output: []interface{}{},
-		},
-	}
-
-	for _, result := range results {
-		root := NewTreeNodes(result.input...)
-		orderList := InorderTraversal(root)
-		if !listInterfaceEqual(orderList, result.output) {
-			t.Errorf("in_order_traversal not qual. input:%+v orderlist:%+v output:%v",
-				result.input, orderList, result.output)
-		}
-	}
-
-}
-
-func TestPostorderTraversal(t *testing.T) {
-
-	results := []*orderResult{
-		{
-			input:  []interface{}{1, nil, 2, nil, nil, 3},
-			output: []interface{}{3, 2, 1},
-		},
-		{
-
-			input:  []interface{}{1},
-			output: []interface{}{1},
-		},
-		{
-
-			input:  []interface{}{},
-			output: []interface{}{},
-		},
-	}
-
-	for _, result := range results {
-		root := NewTreeNodes(result.input...)
-		orderList := PostorderTraversal(root)
-		if !listInterfaceEqual(orderList, result.output) {
-			t.Errorf("post_order_traversal not qual. input:%+v orderlist:%+v output:%v",
-				result.input, orderList, result.output)
-		}
-	}
-
 }
