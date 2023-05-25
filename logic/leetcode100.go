5a5ffae6bac12c0a9a9125bbe54c1cc50884b7fa
wenruo95 <1358124684@qq.com>
2023-05-25 14:42:40 +0800

    --feat=leetcode 100/101

--- logic/leetcode100.go
+++ logic/leetcode100.go
@@ -13,3 +13,85 @@ func IsSameTree(p *TreeNode, q *TreeNode) bool {
 	}
 	return IsSameTree(p.Left, q.Left) && IsSameTree(p.Right, q.Right)
 }
+
+// leetcode 101: https://leetcode.com/problems/symmetric-tree/
+func IsSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	var check func(p, q *TreeNode) bool
+	check = func(p, q *TreeNode) bool {
+		if p == nil && q == nil {
+			return true
+		}
+		if p == nil || q == nil {
+			return false
+		}
+		if p.Val != q.Val {
+			return false
+		}
+		return check(p.Left, q.Right) && check(p.Right, q.Left)
+	}
+
+	return check(root.Left, root.Right)
+}
+
+// leetcode 102: https://leetcode.com/problems/binary-tree-level-order-traversal/
+func LevelOrder(root *TreeNode) [][]int {
+	res := make([][]int, 0)
+
+	list := make([]*TreeNode, 0)
+	list = append(list, root)
+	for len(list) > 0 {
+		list2 := make([]*TreeNode, 0)
+		valList := make([]int, 0)
+
+		var valid bool
+		for _, v := range list {
+			if v == nil {
+				continue
+			}
+			valList = append(valList, v.Val)
+			valid = valid || v.Left != nil || v.Right != nil
+			list2 = append(list2, v.Left, v.Right)
+		}
+
+		if len(valList) > 0 {
+			res = append(res, valList)
+		}
+		if !valid {
+			break
+		}
+		list = list2[0:]
+	}
+
+	return res
+}
+
+func LevelOrder2(root *TreeNode) [][]int {
+	res := make([][]int, 0)
+	if root == nil {
+		return res
+	}
+
+	queue := NewQueue()
+	queue.Push(root)
+	for queue.Top() != nil {
+		size := queue.Size()
+		list := make([]int, 0)
+		for i := 0; i < size; i++ {
+			node := queue.Pop().(*TreeNode)
+			list = append(list, node.Val)
+			if node.Left != nil {
+				queue.Push(node.Left)
+			}
+			if node.Right != nil {
+				queue.Push(node.Right)
+			}
+		}
+		res = append(res, list)
+	}
+
+	return res
+}
--- logic/leetcode100_test.go
+++ logic/leetcode100_test.go
@@ -39,3 +39,59 @@ func TestIsSameTree(t *testing.T) {
 		assert.Equal(t, data.same, same)
 	}
 }
+
+func TestIsSymmetric(t *testing.T) {
+	type testData struct {
+		list      []interface{}
+		symmetric bool
+	}
+
+	datas := []*testData{
+		{
+			list:      []interface{}{1, 2, 2, 3, 4, 4, 3},
+			symmetric: true,
+		},
+		{
+			list:      []interface{}{1, 2, 2, nil, 3, nil, 3},
+			symmetric: false,
+		},
+	}
+
+	for _, data := range datas {
+		list := genTreeNode(data.list)
+		symmetric := IsSymmetric(list)
+		assert.Equal(t, data.symmetric, symmetric)
+	}
+}
+
+func TestLevelOrder(t *testing.T) {
+
+	type testData struct {
+		list []interface{}
+		res  [][]int
+	}
+
+	datas := []*testData{
+		{
+			list: []interface{}{3, 9, 20, nil, nil, 15, 7},
+			res:  [][]int{{3}, {9, 20}, {15, 7}},
+		},
+		{
+			list: []interface{}{1},
+			res:  [][]int{{1}},
+		},
+		{
+			list: []interface{}{},
+			res:  [][]int{},
+		},
+	}
+
+	for _, data := range datas {
+		node := genTreeNode(data.list)
+		res := LevelOrder(node)
+		assert.Equal(t, data.res, res)
+		res2 := LevelOrder(node)
+		assert.Equal(t, data.res, res2)
+	}
+
+}
